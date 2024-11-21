6f2667aff733f9692dcbea7f60e1e2d2381db90b
cyberNKYX <pinnipeds@outlook.com>
2024-11-21 17:28:32 +0800

    添加social link和post外链

--- www/_config.ts
+++ www/_config.ts
@@ -7,6 +7,29 @@ const site = lume({
   src: "./src",
 });
 
+site.data("social_links", [
+  {
+    url: "https://x.com/cyberNKYX",
+    icon: "x-logo",
+    title: "Follow me on X.com"
+  },
+  {
+    url: "https://github.com/cyberNKYX",
+    icon: "github-logo",
+    title: "Find me on GitHub"
+  },
+  {
+    url: "https://patreon.com/cyberNKYX",
+    icon: "patreon-logo",
+    title: "Become a fan on Patreon"
+  },
+  {
+    url: "https://buymeacoffee.com/cybernkyx",
+    icon: "pizza",
+    title: "Buy me a piece of pizza"
+  },
+]);
+
 site.use(theme());
 site.use(ogimages());
 site.use(metas());
--- www/src/SaaS/neon_bookmarks.page.ts
+++ www/src/SaaS/neon_bookmarks.page.ts
@@ -1,6 +1,12 @@
 import { loadMarkdown } from "../../helpers.ts";
 
 export const title = "Neon Bookmarks";
+export const external_links = [
+    { title: "Check source codes on GitHub", url: "https://github.com/cyberNKYX/neon_bookmarks", icon: "github-logo" },
+    { title: "Become fan of Neon bookmarks on Patreon", url: "https://www.patreon.com/posts/supercharge-your-115451366", icon: "patreon-logo" },
+    { title: "Available on the Chrome Web Store", icon: "puzzle-piece", url: "https://chromewebstore.google.com/detail/neon-bookmarks/boohehmkdinflfjbkcbakkfdpihfdpem" },
+    { title: "Buy the dev a piece of pizza", icon: "pizza", url: "https://buymeacoffee.com/cybernkyx"}
+];
 
 export default async function (data: Lume.Data, helpers: Lume.Helpers) {
     const content = await loadMarkdown("https://raw.githubusercontent.com/cyberNKYX/neon_bookmarks/main/README.md")
